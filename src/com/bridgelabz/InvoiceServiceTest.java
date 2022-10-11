29a9aaac0969d56ffa026e397ab9273106c678d4
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-10-11 14:03:49 +0530

    added:welcome to cab invoice generator

---
+++ src/com/bridgelabz/InvoiceServiceTest.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+import org.junit.Assert;
+import org.junit.Test;
+public class InvoiceServiceTest {
+
+        @Test
+        public void givenDistanceAndTime_ShouldReturnTotalFare() {
+            CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+            double distance = 2.0;
+            int time = 5;
+            double totalFare = invoiceService.CalculateFare(distance, time);
+            Assert.assertEquals(25, totalFare,0);
+        }
+
+        @Test
+        public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+            CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+            double distance = 0.1;
+            int time = 1;
+            double totalFare = invoiceService.CalculateFare(distance, time);
+            Assert.assertEquals(5.0, totalFare,0);
+        }
+}
